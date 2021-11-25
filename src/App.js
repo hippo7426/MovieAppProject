380add43d84e6608320b1801723392a05f63853e
SeungJun <hippo7426@gmail.com>
2021-11-25 22:08:05 +0900

    Add TV page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Home from './components/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
 
 function App(){
-  return <div className="container">
-    <Home/>
-    </div>;
+  return <BrowserRouter basename="/SJflix">
+  <Routes>
+    <Route path="/" element={< Home field="movie" />} />
+    <Route path="TV" element={< Home field="TV" />} />
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,70 +5,107 @@ import axios from 'axios';
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            movies: [],
-            category:["Popular", "Upcoming", "Top Rated",]
-         };
-    } 
-   
+        this.state = {
+            contents: [],
+        };
+    }
+    categoryMovie = ["Popular", "Upcoming", "Top Rated"];
+    categoryTV = ["Popular", "On Air", "Top Rated"];
+
     getMovies = async () => {
-        const movies=[];
+        const movies = [];
         const { data: {
-            results : popular
+            results: popular
         } } = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=219db60224db73e0c3bf1948f3e9a86a&language=ko-KR&page=1");
         movies.push(popular);
 
         const { data: {
-            results : upComing
-        }} = await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=219db60224db73e0c3bf1948f3e9a86a&language=ko-KR&page=2");
+            results: upComing
+        } } = await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=219db60224db73e0c3bf1948f3e9a86a&language=ko-KR&page=2");
         movies.push(upComing);
 
-        const { data : {
-            results : top
-        }} = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=219db60224db73e0c3bf1948f3e9a86a&language=ko-KR&page=2");
+        const { data: {
+            results: top
+        } } = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=219db60224db73e0c3bf1948f3e9a86a&language=ko-KR&page=2");
         movies.push(top);
-        this.setState({movies});
+        this.setState({ contents : movies });
     }
 
+    getTVs = async () => {
+        const TVs = [];
+        const { data: {
+            results: popular
+        } } = await axios.get("https://api.themoviedb.org/3/tv/popular?api_key=219db60224db73e0c3bf1948f3e9a86a&language=ko-KR&page=1");
+        TVs.push(popular);
+
+        const { data: {
+            results: upComing
+        } } = await axios.get("https://api.themoviedb.org/3/tv/on_the_air?api_key=219db60224db73e0c3bf1948f3e9a86a&language=ko-KR&page=2");
+        TVs.push(upComing);
+
+        const { data: {
+            results: top
+        } } = await axios.get("https://api.themoviedb.org/3/tv/top_rated?api_key=219db60224db73e0c3bf1948f3e9a86a&language=ko-KR&page=2");
+        TVs.push(top);
+        this.setState({ contents : TVs });
+    }
 
     componentDidMount() {
-        this.getMovies();
+        const field = this.props.field;
+        if (field === "movie")
+            this.getMovies();
+        else if (field === "TV")
+            this.getTVs();
     }
     render() {
+        const field = this.props.field;
+        console.log(field);
         return (
             // 'children' props 를 사용하여 수정 가능
             <div className="home_wrapper">
-                {this.state.category.map((title, index)=>{
-                    console.log(title);
-                    return <HomeSection key={title} category={title} movies={this.state.movies[index]} />
-                }, this)}
+                {/* {    this.categoryMovie.map((title, index) => {
+                            return <HomeSection key={title} category={title} contents={this.state.movies[index]} />
+                        }, this)} */} 
+                {
+                 (() => {
+                    if (field === "movie") {
+                        return this.categoryMovie.map((title, index) => {
+                            console.log(index);
+                            return <HomeSection key={title} category={title} contents={this.state.contents[index]} />
+                        }, this);
+                    } else if (field === "TV") {
+                        return this.categoryTV.map((title, index) =>{
+                            return <HomeSection key={title} category={title} contents={this.state.contents[index]} />
+                        }, this);
+                        } 
+                })()
+                }
+
             </div>
         )
     }
 }
 
 function HomeSection(props) {
-
     return (
         <div className="home_section">
             <HomeCategory title={props.category} />
-            <HomeMovies movies={props.movies}/>
+            <HomeContents contents={props.contents} />
         </div>
 
     )
 }
 
 function HomeCategory(props) {
+    return <div className="contents_category">{props.title ? props.title : "N/A"}</div>;
+}
 
-    return <div className="movies_category">{props.title?props.title:"N/A"}</div>;
-} 
-
-function HomeMovies(props) {
-    console.log(props.movies);
-    return (<div className="movies">
-        {props.movies && props.movies.map(movie=>{
-            return <img className="movies_poster" key={movie.id} alt={movie.title}
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} height="300px"/>;
+function HomeContents(props) {
+    console.log(props.contents);
+    return (<div className="contents">
+        {props.contents && props.contents.map(content => {
+            return <img className="contents_poster" key={content.id} alt={content.title}
+                src={`https://image.tmdb.org/t/p/w500${content.poster_path}`} height="300px" />;
         })}
     </div>);
 }
